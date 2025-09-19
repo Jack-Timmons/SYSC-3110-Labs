b2a7fd150664d2fd694117fa209a7ee641f24c70
TheFloatingHead <jacktimmons10@gmail.com>
2025-09-19 14:50:11 -0400

    main updates

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -15,6 +15,9 @@ public class AddressBook {
     }
 
     public static void main(String[] args){
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Dave", "Carleton", "654");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
