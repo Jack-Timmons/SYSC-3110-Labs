f4ae4b2902d07ea600ea45a20cc4ef69c5004d76
TheFloatingHead <jacktimmons10@gmail.com>
2025-09-26 14:44:28 -0400

    main updates

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        BuddyInfo buddy = new BuddyInfo("Dave", "Carleton", "654");
-        AddressBook addressBook = new AddressBook();
-        addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+
     }
 }
